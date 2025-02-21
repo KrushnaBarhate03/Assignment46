74bf2d2518f7bfa208aa5b6048be02c955e73586
KrushnaBarhate03 <krushnabarhate822@gmail.com>
2025-02-21 21:44:17 +0530

    Error solves

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css'
 import { websiteName,description,imageurl,button1,button2 } from './config';
-import{shopName,mobiledes,mobileimageurl,btn1,btn2}from'./config';
+/*import{shopName,mobiledes,mobileimageurl,btn1,btn2}from'./config';*/
 
 
 function App() {
@@ -10,7 +10,8 @@ function App() {
     <div>
       <h1 className='text-4xl my-5 mx-10'>{websiteName}</h1>
       <p className='text-sm mx-10 my-5'>{description}</p>
-      <img src={imageurl}  height="400" className='h-[300px] object-cover block mx-auto'/>
+      <img src={imageurl} height="400" className='h-[300px] object-cover block mx-auto' alt="sweet" 
+/>
 
       <div className='flex justify-center'>
         <button className='bg-orange-500 m-5 p-1 text-white rounded md'>{button1}</button>
